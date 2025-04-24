8c321733998b647e4063937087a7e825cf6fb5bc
Arthur Fujii <arthurfujii@gmail.com>
2025-04-23 21:59:38 -0300

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
 };
 
-module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placecats.com",
-        port: "",
-        search: "",
-      },
-    ],
-  },
-};
+module.exports = nextConfig;
 
 export default nextConfig;
