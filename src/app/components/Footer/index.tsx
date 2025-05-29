939e305f7e937cc1a1e555a917fa2c940d1599d7
Arthur Fujii <arthurfujii@gmail.com>
2025-05-29 17:34:28 -0300

    feat: add condition to copyright

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className='flex w-full justify-around gap-5 sm:justify-between'>
+    <footer className='flex w-full justify-between gap-5 pb-4'>
       <a
         href='mailto:contato@yzadvogados.com.br'
         target='_blank'
@@ -9,9 +9,14 @@ export function Footer() {
       >
         <small>contato@yzadvogados.com.br</small>
       </a>
-      <small className='text-right'>
-        &copy; 2025. Todos os direitos reservados.
-      </small>
+      {
+        <>
+          <small className='text-right sm:hidden'>&copy; 2025.</small>
+          <small className='hidden text-right sm:block'>
+            &copy; 2025. Todos os direitos reservados.
+          </small>
+        </>
+      }
       {/* <small>+55 11 98765-4321</small> */}
     </footer>
   );
